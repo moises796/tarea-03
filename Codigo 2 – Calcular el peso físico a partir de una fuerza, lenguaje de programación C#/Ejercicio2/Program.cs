4254b191be6294a97715ae96f2345f4672c7207e
moises796 <moisesrivera796@gmail.com>
2018-03-20 16:25:18 -0600

    EjercicioDos

---
+++ Codigo 2 – Calcular el peso físico a partir de una fuerza, lenguaje de programación C#/Ejercicio2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double gravedad=9.8;
+            double fuerza,masa;
+            Console.WriteLine("PROGRAMA PARA CALCULAR PESO DE MAQUINA DEL TIEMPO");
+            Console.WriteLine("");
+            Console.WriteLine("¿A CUANTA FUERZA EN NEWTON ES SOMETIDA LA MAQUINA DEL TIEMPO?");
+            fuerza = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
+            masa = fuerza / gravedad;
+            Console.WriteLine("EL PESO DE LA MAQUINA DE TIEMPO ES DE: "+Math.Round(masa,2)+"kg");
+            Console.ReadKey();
+
+        }
+    }
+}
